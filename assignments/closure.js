edd3be97562b047406cec4e7a88d95deec70e1ac
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-01-22 16:52:28 -0500

    mvp complete
    added closure stretch

--- assignments/closure.js
+++ assignments/closure.js
@@ -13,9 +13,6 @@ const counter = () => {
   return () => counter++;
 };
 const newCounter = counter();
-console.log(newCounter());
-console.log(newCounter());
-console.log(newCounter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
@@ -27,4 +24,17 @@ const counterFactory = () => {
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
+
+  let counter = 0;
+
+  return {
+    increment: () => ++counter,
+    decrement: () => --counter
+  };
 };
+
+const counterObj = counterFactory();
+console.log(counterObj.increment());
+
+const counterObj2 = counterFactory();
+console.log(counterObj2.increment());
